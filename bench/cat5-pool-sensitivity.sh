b80997500c1dccecfb386f343f53a271e89c1c2f
Kirill Stepanishin <kirill.stepanishin@improving.com>
2026-06-01 16:04:18 -0700

    Reduce request counts to 100K for faster runs

--- bench/cat5-pool-sensitivity.sh
+++ bench/cat5-pool-sensitivity.sh
@@ -19,7 +19,7 @@ for POOL in 64 128 256 512 1000 2000 5000; do
     testType 1 \
     host "$BENCH_HOST" \
     parallelism 16 \
-    requests 500000 \
+    requests 100000 \
     executions $EXECUTIONS \
     warmups $WARMUPS \
     maxConnectionPoolSize $POOL
